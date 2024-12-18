45c178c4d6fb001a1a5151d06e5c34d1b04c3050
Karen-404 <tat.3012.mc@gmail.com>
2024-12-18 13:49:12 -0500

    hola

---
+++ Practica123/src/main/java/ec/edu/espoch/practica123/Practica123.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.practica123;
+
+/**
+ *
+ * @author SO-LAB1-PC24
+ */
+public class Practica123 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
