649f6c3cb6951e8da465288cc66e206aa5151e85
Dilankl30 <lucerodk7@gmail.com>
2024-12-18 14:56:25 -0500

    Ahorros

--- src/main/java/ec/edu/espoch/practicas/Practicas.java
+++ src/main/java/ec/edu/espoch/practicas/Practicas.java
@@ -1,10 +1,9 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.espoch.practicas;
 
-import ec.edu.espoch.practicas.clases.Cuenta;
+import ec.edu.espoch.practicas.clases.CuentaCorrientes;
 import ec.edu.espoch.practicas.clases.CuentasAhorros;
 
 /**
@@ -14,8 +13,22 @@ import ec.edu.espoch.practicas.clases.CuentasAhorros;
 public class Practicas {
 
     public static void main(String[] args) {
-   
-    
-    }
+        // Crear una cuenta de ahorros
+        CuentasAhorros cuentaAhorros = new CuentasAhorros(15000, 3.5f);
 
+        cuentaAhorros.consignar(5000);
+        cuentaAhorros.retirar(3000);
+        cuentaAhorros.extractoMensual();
+        cuentaAhorros.imprimir();
+
+        System.out.println("\n");
+
+        // Crear una cuenta corriente
+        CuentaCorrientes cuentaCorriente = new CuentaCorrientes(5000, 4.0f);
+
+        cuentaCorriente.retirar(6000);
+        cuentaCorriente.consignar(2000);
+        cuentaCorriente.extractoMensual();
+        cuentaCorriente.imprimir();
+    }
 }
--- src/main/java/ec/edu/espoch/practicas/clases/CuentasAhorros.java
+++ src/main/java/ec/edu/espoch/practicas/clases/CuentasAhorros.java
@@ -8,6 +8,49 @@ package ec.edu.espoch.practicas.clases;
  *
  * @author USUARIO
  */
-public class CuentasAhorros {
-    
+public class CuentasAhorros extends Cuenta {
+
+    private boolean cuentaActiva;
+
+    public CuentasAhorros(float saldo, float tasaAnual) {
+        super(saldo, tasaAnual);
+        this.cuentaActiva = saldo >= 10000;
+    }
+
+    @Override
+    public void consignar(float cantidad) {
+        if (cuentaActiva) {
+            super.consignar(cantidad);
+        }
+        verificarEstadoCuenta();
+    }
+
+    @Override
+    public boolean retirar(float cantidad) {
+        if (cuentaActiva && super.retirar(cantidad)) {
+            verificarEstadoCuenta();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void extractoMensual() {
+        if (nRetiros > 4) {
+            comiMensual += (nRetiros - 4) * 1000;
+        }
+        super.extractoMensual();
+        verificarEstadoCuenta();
+    }
+
+    private void verificarEstadoCuenta() {
+        cuentaActiva = saldo >= 10000;
+    }
+
+    @Override
+    public void imprimir() {
+        super.imprimir();
+        System.out.println("Cuenta activa: " + cuentaActiva);
+    }
+
 }
